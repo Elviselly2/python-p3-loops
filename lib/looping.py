f84f9c73df2cc2b026fc1d0b3a47f9482f242a0a
nosmall <elvisotieno24@gmail.com>
2025-05-06 13:00:21 +0300

    adds content

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
+    count=10
+    while count >= 1:
+        print (count)
+        count-= 1
+        print('Happy New Year!')
     pass
 
-def square_integers(int_list):
+def square_integers(numbers):
     # code goes here!
+    return  [num **2 for num in numbers]
     pass
 
 def fizzbuzz():
     # code goes here!
-    pass
+   for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+            pass
